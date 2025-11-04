24017da96f53524a80c4a4883354476ca886a0dd
Asti22 <uun3461@gmail.com>
2025-11-04 10:24:00 +0700

    mengganti kalimat di bagian pornya

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${3000}`);
+    console.log(`Server is running on 3000`);
     });
 db.sequelize.sync().then(() => {
     console.log("Database synchronized");
